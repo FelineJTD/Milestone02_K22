ef9b336bdbb2fe623f2121f476a2036447422574
Voguelish <voguelish7@gmail.com>
2021-08-14 01:10:03 +0700

    Back-end, masih WIP

---
+++ Back-End (WIP)/run.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, url_for, flash, redirect, session, request, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html') 
+
+@app.route("/login")
+def login():
+    return render_template('login_miles.html', title='Login')
+
+class InvestorRegister(Form):
+    email = StringField('Email')
+    forename = StringField('Forename')
+    surname = StringField('Surname')
+    company = StringField('Company/Organization')
+    phone = StringField('Phone Number')
+    gender = StringField('Gender')
+    category = StringField('Category Preference')
+    budget = StringField('Investing Budget')
+    startuplocation = StringField('Location of Startup')
+    numberofpeople = StringField('Number of People in the Team')
+    stage = StringField('Development Stage')
+    returntype = StringField('Return Type')    
+
+@app.route("/regis_investor", methods=['GET', 'POST'])
+def regis_investor():
+    form = InvestorRegister(request.form)
+    if request.method == 'POST' and form.validate():
+
+    
+
+    return render_template('regis_investor.html', form=form)
+
+@app.route("/regis_investor/preference")
+def pref():
+    return render_template('pref_investor.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
